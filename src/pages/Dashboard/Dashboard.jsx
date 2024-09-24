074de154b27cee7d5b0eb3e4d6d91008e54821e1
Manjeet <manjeet010@gmail.com>
2024-09-24 23:24:35 +0530

    added dashboard intial configuration

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,16 +3,53 @@ import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid2";
 import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
 export default function Dashboard() {
+  const [type, setType] = useState("today");
+  const [taskList, setTaskList] = useState([]);
+
+  useEffect(() => {
+    console.log("experiment");
+    const token = localStorage.getItem("token");
+    const url = `http://localhost:5000/api/Task/GetAllTask?type=${type}`;
+    fetch(url, {
+      method: "Get",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("experiment" + data);
+        setTaskList(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <Grid container spacing={2} alignItems={"stretch"} height={"100vh"}>
       <Grid size={2} sx={{ background: "#f5f5f5" }}>
-        <Typography variant="subtitle1">
-          <strong>Menu</strong>
-        </Typography>
+        <h3>
+          <span>Menu</span>
+        </h3>
+        <h6>
+          <span>Upcoming</span>
+          <br />
+          <span>Today</span>
+          <br />
+          <span>Calender</span>
+          <br />
+        </h6>
       </Grid>
-      <Grid size={6}>2</Grid>
+      <Grid size={6}>
+        {taskList.map(({ title, iscomplete }, index) => {
+          return <p>{title}</p>;
+        })}
+      </Grid>
       <Grid size={4} sx={{ background: "#f5f5f5" }}>
         3
       </Grid>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,27 +1,17 @@
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  
 
   useEffect(() => {
     const token = localStorage.getItem("token");
+    console.log(token);
     if (token) navigate("/dashboard");
   }, [navigate]);
 
@@ -34,74 +24,60 @@ export default function Login() {
   };
 
   const handleSubmit = () => {
-    const { isPending, error, data, isFetching } = useQuery({
-      queryKey: ["repoData"],
-      queryFn: async () => {
-        const response = await fetch("http://localhost:5000/api/auth", {
-          method: "POST",
-          body: JSON.stringify({ username, password }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        return await response.json();
+    fetch("http://localhost:5000/api/auth", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: {
+        "Content-Type": "application/json",
       },
-    });
-
-  //   fetch("http://localhost:5000/api/auth", {
-  //     method: "POST",
-  //     body: JSON.stringify({ username, password }),
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   })
-  //     .then((res) => res.json())
-  //     .then((res) => {
-  //       if (res.token) {
-  //         localStorage.setItem("token", res.token);
-  //         navigate("/dashboard");
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       console.error(err);
-  //     });
-  // };
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.token) {
+          debugger;
+          localStorage.setItem("token", res.token);
+          console.log(res.token);
+          navigate("/dashboard");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <Box
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          gap="30px"
-          flexDirection="column"
-          height="100vh"
-          maxWidth="250px"
-          margin={"auto"}
-        >
-          <TextField
-            fullWidth
-            id="username"
-            label="Username"
-            variant="outlined"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-          <TextField
-            fullWidth
-            id="password"
-            label="Password"
-            variant="outlined"
-            type="password"
-            onChange={handlePasswordChange}
-          />
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        gap="30px"
+        flexDirection="column"
+        height="100vh"
+        maxWidth="250px"
+        margin={"auto"}
+      >
+        <TextField
+          fullWidth
+          id="username"
+          label="Username"
+          variant="outlined"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+        <TextField
+          fullWidth
+          id="password"
+          label="Password"
+          variant="outlined"
+          type="password"
+          onChange={handlePasswordChange}
+        />
 
-          <Button variant="contained" fullWidth onClick={handleSubmit}>
-            Submit
-          </Button>
-        </Box>
-      </QueryClientProvider>
+        <Button variant="contained" fullWidth onClick={handleSubmit}>
+          Submit
+        </Button>
+      </Box>
     </>
   );
 }
